72cd15368f941dcb92a442da96b627671c70d5d8
Andres100 <aibdj_@hotmail.com>
2015-03-04 21:57:32 -0600

    Cronometro y reloj

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: MARTIN
+ * Date: 04/03/2015
+ * Time: 09:34 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System;
+using System.Threading;
+
+namespace Practica_2
+{ 
+	class Program 
+	{ 
+		static void Main(string[] args) 
+		{
+			Console.Title = "Practica2"; 
+		
+			int hora,minuto,segundo; 
+			 
+			Console.WriteLine("Programa muestra cronometro y reloj \n\n Selecciona una opcion: \n"); 
+			Console.WriteLine("1= Cronometro\n2= Reloj\n"); 
+
+			int opc = int.Parse(Console.ReadLine()); 
+
+			switch (opc) { 
+			
+			case 1: 
+					for (hora=0; hora<60; hora++)
+						for (minuto=0; minuto<60; minuto++)
+							for (segundo=0; segundo<60; segundo++) {
+								Console.Clear ();
+								Console.WriteLine ("Cronometro: \n");
+								Console.WriteLine (" " + hora + ":" + minuto + ":" + segundo); 
+								Thread.Sleep (1000);
+								}
+
+			break;
+
+			case 2:
+				do {
+					Console.Clear ();
+					Console.WriteLine ("Reloj: \n"); 
+					DateTime fecha = DateTime.Now;
+					Console.Write (fecha.Hour + ":" + fecha.Minute + ":" + fecha.Second); 
+					Thread.Sleep (1000);
+				} while(true);
+
+			 break; 
+
+			} 
+
+			Console.ReadKey(true);
+		} 
+	} 
+}
